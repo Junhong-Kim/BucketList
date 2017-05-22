d756e18171069b52984097cfd338e2c62a3d45f1
INMA <jhk1005jhk@gmail.com>
2017-05-22 23:41:40 +0900

    0.5.6 - Added Splash Activity

---
+++ app/src/main/java/com/kimjunhong/bucketlist/activity/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.kimjunhong.bucketlist.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.kimjunhong.bucketlist.R;
+
+/**
+ * Created by INMA on 2017. 5. 22..
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 2000);
+    }
+}
