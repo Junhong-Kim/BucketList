473e7491b25f1f15a2f5840affb10b97e0f450d3
Junhong-Kim <jhk1005jhk@gmail.com>
2017-05-27 13:02:41 +0900

    0.5.9 - Added TedPermission that is permission check library

--- app/src/main/java/com/kimjunhong/bucketlist/activity/MainActivity.java
+++ app/src/main/java/com/kimjunhong/bucketlist/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.kimjunhong.bucketlist.activity;
 
+import android.Manifest;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
@@ -16,6 +17,8 @@ import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
 import com.kimjunhong.bucketlist.R;
 import com.kimjunhong.bucketlist.adapter.TabPagerAdapter;
 import com.kimjunhong.bucketlist.common.BackPressCloseHandler;
@@ -54,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
 
         initViewPager();
         initView();
+        permissionCheck();
     }
 
     @Override
@@ -156,4 +160,24 @@ public class MainActivity extends AppCompatActivity {
         }, 2000);
     }
 
+    private void permissionCheck() {
+        PermissionListener permissionListener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+
+            }
+
+            @Override
+            public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+                Toast.makeText(MainActivity.this, "거절된 권한\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        new TedPermission(this)
+                .setPermissionListener(permissionListener)
+                .setRationaleMessage("원활한 서비스 이용을 위해\n앱의 권한이 필요합니다!\n\n- 개발자 올림 -")
+                .setDeniedMessage("앗.. 권한을 거부하셨어요!\n[설정] ► [권한]에서 권한을 허용하시면\n원활한 서비스 이용이 가능합니다.")
+                .setPermissions(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .check();
+    }
 }
